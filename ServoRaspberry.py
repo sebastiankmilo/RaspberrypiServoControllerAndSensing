b6c18fcfea8fd1755eefdacb7605afa1b233d348
sebastian cruz dussan <sebastiankmilo1996@gmail.com>
2019-05-23 03:21:31 -0500

    defini la clase servo en otro scrip para mejor organización y lo importe en el de prueba

---
+++ ServoRaspberry.py
@@ -0,0 +1,15 @@
+class Servo(object):
+  
+  def  __init__(self,puerto):
+    GPIO.setup(puerto,GPIO.OUT) #Ponemos el pin puerto como el modo deseado(salida o entrada)
+    self.p = GPIO.PWM(puerto,50)
+    self.p.start(7.5) #Enviamos un pulso del 7.5% para centrar el servo
+    pass
+  def angle(self, grados):
+    porcentaje = grados
+    self.p.ChangeDutyCycle(porcentaje)
+    pass
+  def stop(self):
+    self.p.stop()
+    pass
+  pass
--- testcontrolarServos.py
+++ testcontrolarServos.py
@@ -1,23 +1,9 @@
 
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
+import ServoRaspberry.Servo as Servo
 
 
-class Servo(object):
-  
-  def  __init__(self,puerto):
-    GPIO.setup(puerto,GPIO.OUT) #Ponemos el pin puerto como el modo deseado(salida o entrada)
-    self.p = GPIO.PWM(puerto,50)
-    self.p.start(7.5) #Enviamos un pulso del 7.5% para centrar el servo
-    pass
-  def angle(self, grados):
-    porcentaje = grados
-    self.p.ChangeDutyCycle(porcentaje)
-    pass
-  def stop(self):
-    self.p.stop()
-    pass
-  pass
 
 
 
