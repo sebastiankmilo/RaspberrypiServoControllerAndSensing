22a035529b9788d7c7de011c94eebbbf0d82023c
sebastian cruz dussan <sebastiankmilo1996@gmail.com>
2019-05-23 02:49:21 -0500

    modifique el codigo original por sus equivalentes a los metodos de las clase creada para un solo servo

--- testcontrolarServos.py
+++ testcontrolarServos.py
@@ -3,31 +3,10 @@ import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
 
 
-GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
-GPIO.setup(8,GPIO.OUT)    #Ponemos el pin 21 como salida
-p = GPIO.PWM(8,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
-print ("hola")
-
-
-try:                 
-    while True:      #iniciamos un loop infinito
- 
-        p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-        time.sleep(0.5)           #pausa de medio segundo
-        p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
-        time.sleep(0.5)           #pausa de medio segundo
-        p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
-        time.sleep(0.5)           #pausa de medio segundo
- 
-except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
-    p.stop()                      #Detenemos el servo 
-    GPIO.cleanup() 
-
-class servo(object):
+class Servo(object):
   
-  def conf(self,puerto,modo):
-    GPIO.setup(puerto,modo) #Ponemos el pin puerto como el modo deseado(salida o entrada)
+  def  __init__(self,puerto):
+    GPIO.setup(puerto,GPIO.OUT) #Ponemos el pin puerto como el modo deseado(salida o entrada)
     self.p = GPIO.PWM(puerto,50)
     self.p.start(7.5) #Enviamos un pulso del 7.5% para centrar el servo
     pass
@@ -35,4 +14,31 @@ class servo(object):
     porcentaje = grados
     self.p.ChangeDutyCycle(porcentaje)
     pass
+  def stop(self):
+    self.p.stop()
+    pass
   pass
+
+
+
+GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
+#GPIO.setup(8,GPIO.OUT)    #Ponemos el pin 21 como salida
+#p = GPIO.PWM(8,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+#p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
+print ("hola")
+servo = Servo(puerto=8)
+
+try:                 
+    while True:      #iniciamos un loop infinito
+ 
+        servo.angle(grados=4.5) #p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+        time.sleep(0.5)           #pausa de medio segundo
+        servo.angle(grados=10.5)#p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+        time.sleep(0.5)           #pausa de medio segundo
+        servo.angle(grados=7.5)#p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
+        time.sleep(0.5)           #pausa de medio segundo
+ 
+except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
+    servo.stop()                      #Detenemos el servo 
+    GPIO.cleanup() 
+
