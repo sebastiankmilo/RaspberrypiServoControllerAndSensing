48356454ddc5285a144b66942dbbe4ad2c7b03a7
sebastian cruz dussan <sebastiankmilo1996@gmail.com>
2019-05-23 02:32:46 -0500

    creo la clase servo, la cual pose el metodo conf, que va a configurar un puerto para que envie envie pulso pwm, con los cuales se control los servos, y el metodo angle, que define a que angulo voy a ubicar el servo

--- testcontrolarServos.py
+++ testcontrolarServos.py
@@ -2,12 +2,14 @@
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
 
-print ("hola")
+
 GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(8,GPIO.OUT)    #Ponemos el pin 21 como salida
 p = GPIO.PWM(8,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
 p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
- 
+print ("hola")
+
+
 try:                 
     while True:      #iniciamos un loop infinito
  
@@ -21,3 +23,16 @@ try:
 except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
     p.stop()                      #Detenemos el servo 
     GPIO.cleanup() 
+
+class servo(object):
+  
+  def conf(self,puerto,modo):
+    GPIO.setup(puerto,modo) #Ponemos el pin puerto como el modo deseado(salida o entrada)
+    self.p = GPIO.PWM(puerto,50)
+    self.p.start(7.5) #Enviamos un pulso del 7.5% para centrar el servo
+    pass
+  def angle(self, grados):
+    porcentaje = grados
+    self.p.ChangeDutyCycle(porcentaje)
+    pass
+  pass
