c103316bec7bc215cd5f540cc40aedf0acfee028
sebastian cruz dussan <sebastiankmilo1996@gmail.com>
2019-05-25 20:32:07 -0500

    quite el ciclo para ver si el servo mantiene su posición final

--- testcontrolarServos.py
+++ testcontrolarServos.py
@@ -1,29 +1,22 @@
 
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
-import time                #Importamos time para poder usar time.sleep
+#import time
+from datetime import datetime, date, time, timedelta                #Importamos time para poder usar time.sleep
 import ServoRaspberry
 
+GPIO.setmode(GPIO.BOARD)     #Ponemos la Raspberry en modo BOARD
+Servo = ServoRaspberry.Servo #Inicializamos el servo 
 
-Servo = ServoRaspberry.Servo
+servo = Servo(puerto=8) #Configuro el puerto 8 para que me mueva un servo
 
-
-GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
-#GPIO.setup(8,GPIO.OUT)    #Ponemos el pin 21 como salida
-#p = GPIO.PWM(8,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
-#p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
-print ("hola")
-servo = Servo(puerto=8)
-
-try:                 
-    while True:      #iniciamos un loop infinito
- 
-        servo.angle(grados=4.5) #p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-        time.sleep(0.5)           #pausa de medio segundo
-        servo.angle(grados=10.5)#p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
-        time.sleep(0.5)           #pausa de medio segundo
-        servo.angle(grados=7.5)#p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
-        time.sleep(0.5)           #pausa de medio segundo
- 
+try:
+    servo.angle(grados=4.5) #p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+    time.sleep(0.5)           #pausa de medio segundo
+    servo.angle(grados=10.5)#p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+    time.sleep(0.5)           #pausa de medio segundo
+    servo.angle(grados=7.5)#p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
+    time.sleep(0.5) 
+    
 except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
     servo.stop()                      #Detenemos el servo 
     GPIO.cleanup() 
