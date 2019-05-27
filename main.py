4a9917ee59fc3c5442d0bb1a637aa9500a476286
sebastian cruz dussan <sebastiankmilo1996@gmail.com>
2019-05-27 14:39:04 -0500

    inicialize los 5 servos, dentro de un array que los contiene, cree las funciones para moverlos y las agregue a la función de leer, antes de recivir los datos y despúes de recivirlos, además cambie el parametro datos, de la función leer por servos, el cual representa el array que contiene los servos

--- main.py
+++ main.py
@@ -5,20 +5,30 @@ import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import ServoRaspberry
 
 # Función que es llamada por los eventos
-def Leer(dato):
-    print('TIEMPO:{}'.format(dato)) ##mover datos
-    comienzo = time.time()
-    tiempoEjecuecion = 5
-    final = comienzo + tiempoEjecuecion
-    cant = 0;
+def Leer(servos):
+    abrir(Servos=servos) ##mover los servos para ponerlos en la posición en la cual puedan tomar datos
+    comienzo = time.time() ##guardo el tiempo en el cual lo servos ya estan en posición para tomar datos
+    tiempoEjecuecion = 5  #defino por cuanto tiempo voy a tomar los datos
+    final = comienzo + tiempoEjecuecion #defino en que momento se deja de tomar los datos
+    cant = 0
     while final > time.time():
       cant = cant +1 #recivir la información del arduino
       pass
-    print (cant)
+    cerrar(Servos=servos) #Muevo los servos
     pass
     
-def abrir(servo):
-  
+def abrir(Servos):
+  for servo in Servos:
+    servo.angle(180)
+    pass  
+  time.sleep(4)
+  pass
+
+def cerrar(Servos):
+  for servo in Servos:
+    servo.angle(90)
+    pass  
+  time.sleep(4)
   pass
 
 
@@ -31,8 +41,19 @@ horarios["mañana"] = mañana
 horarios["tarde"] = tarde
 horarios["noche"] = noche
 GPIO.setmode(GPIO.BOARD)
-servo
+#Servo1 = ServoRaspberry.Servo(puerto = 8)  #Defino donde va a estar conectado el servo 1 a los puertos GPIO  de la raspberrypi
+#Servo2 = ServoRaspberry.Servo(puerto = 9)  #Defino donde va a estar conectado el servo 2 a los puertos GPIO  de la raspberrypi
+#Servo3 = ServoRaspberry.Servo(puerto = 10) #Defino donde va a estar conectado el servo 3 a los puertos GPIO  de la raspberrypi
+#Servo4 = ServoRaspberry.Servo(puerto = 11) #Defino donde va a estar conectado el servo 4 puertos GPIO  de la raspberrypi
+#Servo5 = ServoRaspberry.Servo(puerto = 12) #Defino donde va a estar conectado el servo 5  a los puertos GPIO  de la raspberrypi
 
+puertos=[8,9,10,11,12]
+pos=0
+servos = [0 for i in range(4)] #array q contiene los 4 servos
+for servo in servos:
+  servo = ServoRaspberry.Servo(puerto = puertos[pos])
+  pos=pos+1
+  pass
 
 
 
@@ -46,13 +67,15 @@ while test :
   time.sleep(1)
   horarios["actual"] = tm(datetime.now().hour,datetime.now().minute,datetime.now().second)
   if horarios["actual"] >= horarios["mañana"] :
-    Leer("mañana")
+    print("mañana")
+    Leer(servos=servos)
     mañana = True
     while mañana :
       time.sleep(1)
       horarios["actual"] = tm(datetime.now().hour,datetime.now().minute,datetime.now().second)
       if horarios["actual"] >= horarios["tarde"] :
-        Leer("tarde")
+        print("tarde")
+        Leer(servos=servos)
         mañana = False
         tarde = True
         while tarde:
@@ -61,7 +84,8 @@ while test :
           if horarios["actual"] >= horarios["noche"] :
             tarde = False
             noche = True
-            Leer("noche")
+            Leer(servos)
+            print("noche")
             while noche :
               time.sleep(1)
               horarios["actual"] = tm(datetime.now().hour,datetime.now().minute,datetime.now().second)
